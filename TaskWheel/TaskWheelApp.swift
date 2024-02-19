329e203f31a2ed68165a7ad4c048fc990b072220
dotcommadotcom <byjiwoolee@gmail.com>
2024-02-19 13:08:20 +0900

    Initial Commit

---
+++ TaskWheel/TaskWheelApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskWheelApp.swift
+//  TaskWheel
+//
+//  Created by Jiwoo on 2024-02-19.
+//
+
+import SwiftUI
+
+@main
+struct TaskWheelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
