d7f43961f8515b8f7065cf7218c31406610538f3
dotcommadotcom <byjiwoolee@gmail.com>
2024-02-19 22:51:38 +0900

    add toggle and add task to task view model

--- TaskWheel/ViewModels/TaskViewModel.swift
+++ TaskWheel/ViewModels/TaskViewModel.swift
@@ -3,11 +3,23 @@ import Foundation
 class TaskViewModel {
     @Published var taskList: [TaskModel] = []
     
-    init() {
-        getExamples()
-    }
+//    init() {
+//        getExamples()
+//    }
     
     func getExamples() {
         taskList.append(contentsOf: TaskModel.examples)
+    }
+    
+    func addTask(_ task: TaskModel) {
+        if task.isTitleValid() {
+            taskList.append(task)
+        }
+    }
+    
+    func toggleComplete(_ task: TaskModel) {
+        if let index = taskList.firstIndex(where: { $0.id == task.id }) {
+            taskList[index] = task.toggleComplete()
+        }
     }
 }
--- TaskWheelTests/TaskViewModelTests.swift
+++ TaskWheelTests/TaskViewModelTests.swift
@@ -1,17 +1,53 @@
-//import XCTest
-//@testable import TaskWheel
-//
-//final class TaskViewModelTests: XCTestCase {
-//    
-//    override func setUpWithError() throws {
-//        // Put setup code here. This method is called before the invocation of each test method in the class.
-//    }
-//    
-//    
-//    
-//    func testGetExamples() throws {
-//        let example = TaskViewModel()
-//        
-//        XCTAssertEqual(example.taskList.count, 25)
-//    }
-//}
+import XCTest
+@testable import TaskWheel
+
+final class TaskViewModelTests: XCTestCase {
+    
+    private var simpleTaskList: TaskViewModel!
+    private var multipleTaskList: TaskViewModel!
+    
+    override func setUpWithError() throws {
+        try super.setUpWithError()
+        simpleTaskList = TaskViewModel()
+        multipleTaskList = TaskViewModel()
+        multipleTaskList.getExamples()
+    }
+    
+    override func tearDownWithError() throws {
+        try super.tearDownWithError()
+        simpleTaskList = nil
+        multipleTaskList = nil
+    }
+    
+    func testEmptyTaskList() throws {
+        XCTAssertTrue(simpleTaskList.taskList.isEmpty)
+    }
+    
+    func testAddValidTaskAddsTask() throws {
+        let testTask = TaskModel(title: "this is a test")
+        
+        simpleTaskList.addTask(testTask)
+        
+        XCTAssertEqual(simpleTaskList.taskList.count, 1)
+    }
+    
+    func testAddInvalidTaskDoesNothing() throws {
+        let testTask = TaskModel(title: "")
+        
+        simpleTaskList.addTask(testTask)
+        
+        XCTAssertEqual(simpleTaskList.taskList.count, 0)
+    }
+        
+    func testMultipleTaskList() throws {
+        XCTAssertEqual(multipleTaskList.taskList.count, 25)
+    }
+    
+    func testToggleCompleteChangesTask() throws {
+        let recycleTask = multipleTaskList.taskList[6]
+        
+        multipleTaskList.toggleComplete(recycleTask)
+        
+        XCTAssertTrue(multipleTaskList.taskList[6].isComplete)
+    }
+}
