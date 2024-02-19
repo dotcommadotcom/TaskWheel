5c50edf8c6dd4baedc1c21ae8ea82d05cd64fb86
dotcommadotcom <byjiwoolee@gmail.com>
2024-02-19 13:26:57 +0900

    initialize task model and main view

---
+++ TaskWheel/Models/TaskModel.swift
@@ -0,0 +1,42 @@
+import Foundation
+
+struct TaskModel: Identifiable, Hashable {
+    let id = UUID()
+    let title: String
+    let isComplete: Bool
+    
+    init(title: String, isComplete: Bool = false) {
+        self.title = title
+        self.isComplete = false
+    }
+}
+
+extension TaskModel {
+    static let examples: [TaskModel] = [
+        .init(title: "laundry", isComplete: false),
+        .init(title: "dishes", isComplete: true),
+        .init(title: "vacuum", isComplete: false),
+        .init(title: "mop", isComplete: false),
+        .init(title: "water plants", isComplete: false),
+        .init(title: "throw out trash", isComplete: false),
+        .init(title: "recycle", isComplete: false),
+        .init(title: "wipe countertop", isComplete: false),
+        .init(title: "fold laundry", isComplete: false),
+        .init(title: "clean bathroom", isComplete: false),
+        .init(title: "organize shelf", isComplete: false),
+        .init(title: "wipe mirror", isComplete: false),
+        .init(title: "wipe windowsill", isComplete: false),
+        .init(title: "sanitize toilet brushes", isComplete: false),
+        .init(title: "call water company", isComplete: false),
+        .init(title: "pay bills", isComplete: false),
+        .init(title: "switch out spring clothes", isComplete: false),
+        .init(title: "wipe electronics", isComplete: false),
+        .init(title: "charge all electronics", isComplete: false),
+        .init(title: "refill coffee", isComplete: false),
+        .init(title: "buy milk", isComplete: false),
+        .init(title: "buy bananas", isComplete: false),
+        .init(title: "dry clean", isComplete: false),
+        .init(title: "clean washing machine", isComplete: false),
+        .init(title: "empty vacuum cleaner", isComplete: false),
+    ]
+}
--- TaskWheel/TaskWheelApp.swift
+++ TaskWheel/TaskWheelApp.swift
@@ -1,17 +1,10 @@
-//
-//  TaskWheelApp.swift
-//  TaskWheel
-//
-//  Created by Jiwoo on 2024-02-19.
-//
-
 import SwiftUI
 
 @main
 struct TaskWheelApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            MainView()
         }
     }
 }
