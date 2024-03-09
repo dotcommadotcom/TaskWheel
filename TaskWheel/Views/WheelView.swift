b36459f65ea3c243c66180b891f885f95eb28c8e
dotcommadotcom <byjiwoolee@gmail.com>
2024-03-09 23:52:18 +0900

    format wheel view

--- TaskWheel/Views/WheelView.swift
+++ TaskWheel/Views/WheelView.swift
@@ -1,95 +1,73 @@
 import SwiftUI
+import DequeModule
 
 struct WheelView: View {
     
     @EnvironmentObject var taskViewModel: TaskViewModel
     @State var selected: Int = -1
     @State var rotateBy: CGFloat = 0.0
-    
-    private var numSlices: Int { alphabet.count }
-    private var sliceSize: CGFloat {
-        return 360.0 / CGFloat(numSlices) // Angle in degrees
-    }
-    
-    let alphabet = ["All the complications in the ", "Back to back long texts will look like this", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "This is going to be long", "U", "V", "W", "X", "Y", "Z"]
+
+    var wheelTasks: [String] { taskViewModel.currentTasks().map { $0.title } }
+    var numSlices: Int { wheelTasks.count }
+    var sliceSize: CGFloat { return 360.0 / CGFloat(numSlices) }
     
     var body: some View {
         HStack(alignment: .center, spacing: 0) {
-            wheelCircleView()
-            
-            Icon(this: .ticker, size: 20)
-                .shadow(radius: 3)
-                .padding(.horizontal, -15)
+            wheelCircleView(diameter: 600)
+                .rotationEffect(.degrees(rotateBy))
+                .animation(.easeInOut(duration: 4), value: rotateBy)
             
             spinButton()
-                .padding(25)
+                .padding(.horizontal, -10)
         }
+        .frame(maxWidth: .infinity)
+        .offset(x: -150, y: 0)
+        .scaledToFit()
+        .padding()
     }
 }
- 
+
 extension WheelView {
     
-    private func wheelCircleView() -> some View {
-        GeometryReader { geometry in
-            let diameter: CGFloat = geometry.size.width * 2
+    func wheelCircleView(diameter: CGFloat) -> some View {
+        ZStack {
+            Circle().fill(.gray.opacity(0.2))
             
-            ZStack(alignment: .trailing) {
-                ZStack {
-                    Circle().fill(.gray.opacity(0.2))
-                    
-                    ForEach(alphabet.indices, id: \.self) { index in
-                        WheelSegmentView(
-                            text: alphabet[index],
-                            index: index,
-                            angle: angle(at: index),
-                            textOffset: geometry.size.width - 25,
-                            selectedIndex: $selected
-                        )
-                    }
-                    .rotationEffect(.degrees(rotateBy))
-                    .animation(.easeInOut(duration: 4), value: rotateBy)
-                }
-                .frame(width: diameter, height: diameter)
-                .position(CGPoint(x: 0, y: geometry.size.height / 2))
+            ForEach(wheelTasks.indices, id: \.self) { index in
+                WheelSegmentView(
+                    text: wheelTasks[index],
+                    index: index,
+                    angle: angle(at: index),
+                    textOffset: (0.9 * diameter) / 2,
+                    selectedIndex: $selected
+                )
             }
         }
+        .frame(width: diameter, height: diameter)
     }
     
     private func spinButton() -> some View {
         Button {
             selected = Int.random(in: 0..<numSlices)
-
-            let currentAngle = fmod(rotateBy, 360)
-            let targetAngle = 360 - Double(selected) * 360.0 / Double(numSlices)
-            let angleDifference = fmod(360 - currentAngle + targetAngle, 360)
-
-            // Apply the rotation with animation
+            
             withAnimation(.easeInOut(duration: 6)) {
-                rotateBy += 360 * 5 + angleDifference
+                rotateBy += 360 * 5 + angleDifference(from: selected)
             }
-
+            
         } label: {
-            ZStack {
-                RoundedRectangle(cornerRadius: 25)
-                    .fill(.accent)
-                
-                Text("Spin").foregroundStyle(.textAlt)
-                    .padding(8)
-                    .padding(.horizontal, 8)
-            }
-            .frame(height: 30)
-            .fixedSize()
+            Icon(this: .ticker, size: 20)
         }
     }
-    
+
     private func angle(at index: Int) -> CGFloat {
         return sliceSize * CGFloat(index)
     }
-    
-    private func angleForLetter(at index: Int) -> Double {
-        return Double(index) * 360.0 / Double(numSlices)
+
+    private func angleDifference(from index: Int) -> Double {
+        let currentAngle = fmod(rotateBy, 360)
+        let targetAngle = 360 - Double(selected) * 360.0 / Double(numSlices)
+        return fmod(360 - currentAngle + targetAngle, 360)
     }
-    
 }
 
 struct WheelSegmentView: View {
@@ -105,8 +83,8 @@ struct WheelSegmentView: View {
     var fontSize: CGFloat = 20
     
     var body: some View {
-        let xOffset = cos(angle * .pi / 180.0) * (textOffset - textWidth / 2)
-        let yOffset = sin(angle * .pi / 180.0) * (textOffset - textWidth / 2)
+        let xOffset = cos(angle * .pi / 180.0) * (textOffset - textWidth / 2.0)
+        let yOffset = sin(angle * .pi / 180.0) * (textOffset - textWidth / 2.0)
         
         Text(text)
             .lineLimit(1)
@@ -125,7 +103,6 @@ struct WheelSegmentView: View {
             .fontWeight(selectedIndex == index ? .bold : .regular)
     }
 }
-
 
 #Preview {
     WheelView()
